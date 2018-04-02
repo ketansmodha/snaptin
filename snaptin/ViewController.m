980ed765487421fdc55177402744de18d7869550
rbizwts <rbizwts@gmail.com>
2018-04-02 17:49:25 +0530

    Added viewDidAppear method by KM

--- snaptin/ViewController.m
+++ snaptin/ViewController.m
@@ -19,6 +19,12 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+
+	NSLog(@"Hello View did appear");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
